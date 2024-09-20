8c23fe5a1e6eee333692ae2811cf07fd11d4d0eb
tomasludv <tomasludvik@seznam.cz>
2024-09-20 10:04:50 +0200

    rework

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,10 +5,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 error NotListed(address nftAddress, uint256 tokenId);
-error AlreadyListed(address nftAddress, uint256 tokenId);
 error NotOwner();
-error NotApprovedForMarketplace();
-error PriceMustBeAboveZero();
 
 event ItemListed(
     address indexed seller,
@@ -33,6 +30,7 @@ event ItemBought(
 );
 
 struct Listing {
+    bool active;
     address seller;
     uint256 price;
     address token;
@@ -47,19 +45,8 @@ contract Marketplace {
         uint256 price,
         address token
     ) external {
-        if (price == 0) {
-            revert PriceMustBeAboveZero();
-        }
-        if (msg.sender != IERC721(nft).ownerOf(id)) {
-            revert NotOwner();
-        }
-        if (listings[nft][id].price > 0) {
-            revert AlreadyListed(nft, id);
-        }
-        if (IERC721(nft).getApproved(id) != address(this)) {
-            revert NotApprovedForMarketplace();
-        }
-        listings[nft][id] = Listing(msg.sender, price, token);
+        listings[nft][id] = Listing(true,msg.sender, price, token);
+        IERC721(nft).safeTransferFrom(msg.sender, address(this), id, "");
         emit ItemListed(msg.sender, nft, id, price, token);
     }
 
@@ -69,37 +56,34 @@ contract Marketplace {
         uint256 price,
         address token
     ) external {
-        if (price == 0) {
-            revert PriceMustBeAboveZero();
-        }
-        if (msg.sender != IERC721(nft).ownerOf(id)) {
+        Listing storage listing = listings[nft][id];
+        if (listing.seller != msg.sender) {
             revert NotOwner();
         }
-        if (listings[nft][id].price == 0) {
+        if (listing.active == false) {
             revert NotListed(nft, id);
         }
-        Listing storage listing = listings[nft][id];
         listing.price = price;
         listing.token = token;
         emit ItemListed(msg.sender, nft, id, price, token);
     }
 
     function buyItem(address nft, uint256 id) external {
-        if (listings[nft][id].price == 0) {
+        Listing memory listing = listings[nft][id];
+        if (listings[nft][id].active == false) {
             revert NotListed(nft, id);
         }
-        Listing memory listing = listings[nft][id];
         IERC20(listing.token).transferFrom(
             msg.sender,
             listing.seller,
             listing.price
         );
-        IERC721(nft).safeTransferFrom(listing.seller, msg.sender, id);
+        IERC721(nft).transferFrom(address(this), msg.sender, id);
         delete (listings[nft][id]);
         emit ItemBought(msg.sender, nft, id, listing.price, listing.token);
     }
 
-    function getListing(address nft, uint256 id)
+     function getListing(address nft, uint256 id)
         external
         view
         returns (Listing memory)
